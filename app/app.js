fabe7f6e41278bb49d98357bc652cc42dc4975ba
Christian Tran <christiantran04@gmail.com>
2018-05-08 13:48:14 -0600

    tab info

---
+++ app/app.js
@@ -0,0 +1,9 @@
+console.log(5);
+var app = {
+    controllers: {
+        swapiController: new SwapiController(),
+        classesController: new ClassesController(),
+        racesController: new RacesController()
+    }
+};
+console.log("end");
